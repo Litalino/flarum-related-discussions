b8815e8592f17ab62ec2d8fe916807ef14cc24ad
Litalino <99712477+Litalino@users.noreply.github.com>
2024-01-12 22:34:46 +0700

    Update extend.php

--- extend.php
+++ extend.php
@@ -24,6 +24,7 @@ return [
   (new Extend\Frontend('admin'))
     ->js(__DIR__ . '/js/dist/admin.js')
     ->css(__DIR__ . '/resources/less/admin.less'),
+
   new Extend\Locales(__DIR__ . '/resources/locale'),
 
   (new Extend\Settings())
